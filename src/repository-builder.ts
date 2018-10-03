aed7fd388030ee8f3281e24cf3f6b3fa6ea5bd90
Mitica <canteadumitru@gmail.com>
2018-10-03 20:30:20 +0300

    don't export mongodb types

--- src/repository-builder.ts
+++ src/repository-builder.ts
@@ -1,4 +1,3 @@
-import { Db } from "mongodb";
 import { PhraseModel } from "./phrase-model";
 import { MongoPhraseRepository } from "./mongo-phrase-repository";
 import { PhraseRepository, ReportRepository } from "@ournet/horoscopes-domain";
@@ -8,13 +7,13 @@ import { MongoReportRepository } from "./mongo-report-repository";
 
 
 export class PhraseRepositoryBuilder {
-    static build(db: Db, tableSuffix: string = 'v0'): PhraseRepository {
+    static build(db: any, tableSuffix: string = 'v0'): PhraseRepository {
         return new MongoPhraseRepository(new PhraseModel(db, tableSuffix));
     }
 }
 
 export class ReportRepositoryBuilder {
-    static build(db: Db, tableSuffix: string = 'v0'): ReportRepository {
+    static build(db: any, tableSuffix: string = 'v0'): ReportRepository {
         return new MongoReportRepository(new ReportModel(db, tableSuffix));
     }
 }
