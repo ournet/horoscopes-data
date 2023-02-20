a97b9a9761da5d85928f9b4925457cec2c5f0fdd
Dumitru C <canteadumitru@gmail.com>
2023-02-20 19:08:44 +0200

    v0.1.3

--- src/mongo-phrase-repository.ts
+++ src/mongo-phrase-repository.ts
@@ -1,37 +1,62 @@
-import { BaseRepository, RepositoryAccessOptions, RepositoryUpdateData } from "@ournet/domain";
+import {
+  BaseRepository,
+  RepositoryAccessOptions,
+  RepositoryUpdateData
+} from "@ournet/domain";
 import { PhraseModel } from "./phrase-model";
-import { PhraseValidator, Phrase, PhraseRepository, RandomPhrasesQueryParams } from "@ournet/horoscopes-domain";
+import {
+  PhraseValidator,
+  Phrase,
+  PhraseRepository,
+  RandomPhrasesQueryParams
+} from "@ournet/horoscopes-domain";
+import { PhraseQueryParams } from "@ournet/horoscopes-domain/lib/phrase-repository";
 
-export class MongoPhraseRepository extends BaseRepository<Phrase> implements PhraseRepository {
-    constructor(private model: PhraseModel) {
-        super(new PhraseValidator());
-    }
+export class MongoPhraseRepository
+  extends BaseRepository<Phrase>
+  implements PhraseRepository
+{
+  constructor(private model: PhraseModel) {
+    super(new PhraseValidator());
+  }
+  list(params: PhraseQueryParams) {
+    return this.model.list(params);
+  }
 
-    innerCreate(data: Phrase): Promise<Phrase> {
-        return this.model.create(data);
-    }
-    innerUpdate(data: RepositoryUpdateData<Phrase>): Promise<Phrase> {
-        return this.model.update(data);
-    }
-    delete(id: string): Promise<boolean> {
-        return this.model.delete(id);
-    }
-    getById(id: string, options?: RepositoryAccessOptions<Phrase>): Promise<Phrase | null> {
-        return this.model.getById(id, options);
-    }
-    getByIds(ids: string[], options?: RepositoryAccessOptions<Phrase>): Promise<Phrase[]> {
-        return this.model.getByIds(ids, options);
-    }
-    exists(id: string): Promise<boolean> {
-        return this.model.exists(id);
-    }
-    deleteStorage(): Promise<void> {
-        return this.model.deleteStorage();
-    }
-    createStorage(): Promise<void> {
-        return this.model.createStorage();
-    }
-    random(params: RandomPhrasesQueryParams, options?: RepositoryAccessOptions<Phrase>): Promise<Phrase[]> {
-        return this.model.random(params, options);
-    }
+  innerCreate(data: Phrase): Promise<Phrase> {
+    return this.model.create(data);
+  }
+  innerUpdate(data: RepositoryUpdateData<Phrase>): Promise<Phrase> {
+    return this.model.update(data);
+  }
+  delete(id: string): Promise<boolean> {
+    return this.model.delete(id);
+  }
+  getById(
+    id: string,
+    options?: RepositoryAccessOptions<Phrase>
+  ): Promise<Phrase | null> {
+    return this.model.getById(id, options);
+  }
+  getByIds(
+    ids: string[],
+    options?: RepositoryAccessOptions<Phrase>
+  ): Promise<Phrase[]> {
+    return this.model.getByIds(ids, options);
+  }
+  exists(id: string): Promise<boolean> {
+    return this.model.exists(id);
+  }
+  deleteStorage(): Promise<void> {
+    return this.model.deleteStorage();
+  }
+  createStorage(): Promise<void> {
+    return this.model.createStorage();
+  }
+  random(
+    params: RandomPhrasesQueryParams,
+    options?: RepositoryAccessOptions<Phrase>
+  ): Promise<Phrase[]> {
+    return this.model.random(params, options);
+  }
 }
--- src/phrase-model.ts
+++ src/phrase-model.ts
@@ -1,48 +1,77 @@
 import { MongoModel } from "./mongo-model";
 import { RepositoryUpdateData, RepositoryAccessOptions } from "@ournet/domain";
-import { Phrase, PhraseRepository, RandomPhrasesQueryParams } from '@ournet/horoscopes-domain';
+import {
+  Phrase,
+  PhraseRepository,
+  RandomPhrasesQueryParams
+} from "@ournet/horoscopes-domain";
+import { PhraseQueryParams } from "@ournet/horoscopes-domain/lib/phrase-repository";
 import { Db } from "mongodb";
 
-const RAND_KEY = 'rand';
+const RAND_KEY = "rand";
 
-export class PhraseModel extends MongoModel<Phrase> implements PhraseRepository {
-    constructor(db: Db, tableSuffix: string) {
-        super(db, `ournet_horo_phrases_${tableSuffix}`);
-    }
+export class PhraseModel
+  extends MongoModel<Phrase>
+  implements PhraseRepository
+{
+  constructor(db: Db, tableSuffix: string) {
+    super(db, `ournet_horo_phrases_${tableSuffix}`);
+  }
+  async list({ lang, limit, offset }: PhraseQueryParams): Promise<Phrase[]> {
+    const result = await this.collection
+      .find({ lang })
+      .sort("id")
+      .limit(limit)
+      .skip(offset || 0)
+      .toArray();
 
-    async random(params: RandomPhrasesQueryParams, _options?: RepositoryAccessOptions<Phrase>): Promise<Phrase[]> {
-        const result = await this.collection.aggregate([
-            { $match: { lang: params.lang, sign: params.sign, period: params.period } },
-            { $sample: { size: params.limit } },
-        ]).toArray();
+    return result.map((item) => this.convertFromMongoDoc(item));
+  }
 
-        return result.map(item => this.convertFromMongoDoc(item));
-    }
+  async random(
+    params: RandomPhrasesQueryParams,
+    _options?: RepositoryAccessOptions<Phrase>
+  ): Promise<Phrase[]> {
+    const result = await this.collection
+      .aggregate([
+        {
+          $match: {
+            lang: params.lang,
+            sign: params.sign,
+            period: params.period
+          }
+        },
+        { $sample: { size: params.limit } }
+      ])
+      .toArray();
 
-    async createStorage() {
-        await super.createStorage();
-        await this.collection.createIndex({ lang: 1, period: 1, sign: 1 });
-    }
+    return result.map((item) => this.convertFromMongoDoc(item));
+  }
 
-    protected beforeCreate(data: Phrase) {
-        // (<any>data)[RAND_KEY] = formatRandKey();
+  async createStorage() {
+    await super.createStorage();
+    await this.collection.createIndex({ lang: 1, period: 1, sign: 1 });
+  }
 
-        return super.beforeCreate(data);
-    }
+  protected beforeCreate(data: Phrase) {
+    // (<any>data)[RAND_KEY] = formatRandKey();
 
-    protected beforeUpdate(data: RepositoryUpdateData<Phrase>) {
-        // if (data.set) {
-        //     (<any>data.set)[RAND_KEY] = formatRandKey();
-        // }
+    return super.beforeCreate(data);
+  }
 
-        return super.beforeUpdate(data);
-    }
+  protected beforeUpdate(data: RepositoryUpdateData<Phrase>) {
+    // if (data.set) {
+    //     (<any>data.set)[RAND_KEY] = formatRandKey();
+    // }
 
-    protected convertFromMongoDoc(doc: any): Phrase {
-        delete doc[RAND_KEY];
+    return super.beforeUpdate(data);
+  }
 
-        return super.convertFromMongoDoc(doc);
-    }
+  protected convertFromMongoDoc(doc: any): Phrase {
+    delete doc[RAND_KEY];
+
+    return super.convertFromMongoDoc(doc);
+  }
 }
 
 // function formatRandKey() {
